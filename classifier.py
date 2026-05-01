62d28d2f51b82dc79997eb78b2ecbec0d157d617
Vartika Tomar <vartika.25bai10728@vitbhopal.ac.in>
2026-05-01 17:35:49 +0530

    code for classifier

--- classifier.py
+++ classifier.py
@@ -1,1 +1,18 @@
+def classify_ticket(text):
+    text = text.lower()
 
+    if "visa" in text:
+        domain = "visa"
+    elif "claude" in text:
+        domain = "claude"
+    else:
+        domain = "hackerrank"
+
+    if "payment" in text or "charged" in text:
+        issue = "billing"
+    elif "login" in text:
+        issue = "access"
+    else:
+        issue = "general"
+
+    return domain, issue
